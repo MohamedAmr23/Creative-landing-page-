64e44ec23031fba6b00939cd99523a13b0e22b94
MohamedAmr23 <102430066+MohamedAmr23@users.noreply.github.com>
2023-09-06 02:00:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+alert("صلي علي النبي ")
+let img=document.querySelector(".img")
+
+let container=document.querySelector(".container")
+function phones(phone){
+    img.src=phone
+}
+function colors(color){
+    container.style.background=color
+}
+
